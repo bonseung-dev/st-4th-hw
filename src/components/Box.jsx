a19060a311079b185039d27cd7b02e86c522f17f
bonseung-dev <glynnregister@gmail.com>
2025-02-04 09:59:21 +0900

    ex3

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-// TODO: props로 받은 $active 에 따라 배경색이 blue 또는 gray가 되도록 해보세요.
+// StyledBox라는 스타일을 가진 컴포넌트를 만듭니다.
 const StyledBox = styled.div`
   width: 100px;
   height: 100px;
-  background-color: "gray";
+  background-color: ${(props) =>
+    props.active ? "blue" : "gray"}; // active가 true면 파란색, 아니면 회색
   display: flex;
   align-items: center;
   justify-content: center;
@@ -15,7 +16,11 @@ const StyledBox = styled.div`
 `;
 
 function Box({ active, onClick }) {
-  return <StyledBox $active={active} onClick={onClick} />;
+  return (
+    <StyledBox active={active} onClick={onClick}>
+      {active ? "Active" : "Box"}
+    </StyledBox>
+  );
 }
 
 export default Box;
--- src/components/BoxContainer.jsx
+++ src/components/BoxContainer.jsx
@@ -4,11 +4,11 @@ import Box from "./Box";
 
 const Container = styled.div`
   display: flex;
-  gap: 10px;
+  gap: 10px; // 박스들 사이의 간격
 `;
 
 function BoxContainer() {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(null);
 
   const handleClick = (index) => {
     setActiveIndex(index);
@@ -16,9 +16,12 @@ function BoxContainer() {
 
   return (
     <Container>
-      {/* TODO: active prop 을 어떻게 해야 클릭한 박스임을 알수있을 지 active prop에 할당할 값을 수정해보세요. */}
       {[0, 1, 2, 3, 4].map((index) => (
-        <Box key={index} active={false} onClick={() => handleClick(index)} />
+        <Box
+          key={index}
+          active={activeIndex === index}
+          onClick={() => handleClick(index)}
+        />
       ))}
     </Container>
   );
